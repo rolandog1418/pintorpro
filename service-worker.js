55bfad7ca19bbb254bf6ddadf5d6f361eb924977
rolandog1418 <rolandog1418@gmail.com>
2025-12-24 13:59:45 -0300

    Update service worker to version 2 and enhance caching

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,40 @@
-const CACHE_NAME = 'pintorpro-v1';
+const CACHE_NAME = 'pintorpro-v2';
 
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || new Response(
+        '<h1>Offline</h1>',
+        { headers: { 'Content-Type': 'text/html' } }
+      );
+    })
   );
 });
